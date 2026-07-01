22c6bbe58f46cd4818ff74ae78f76ed766477312
AgostaGiorgio <agogi.dev@gmail.com>
2026-07-01 18:49:49 +0400

    feat: add skeleton for FE

---
+++ frontend/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import AppHeader from './components/AppHeader.vue'
+
+</script>
+
+<template>
+  <div class="min-h-screen bg-brand-background text-brand-textMain font-sans pb-24 selection:bg-brand-primary selection:text-white">    
+    
+    <AppHeader />
+  
+  </div>
+</template>
