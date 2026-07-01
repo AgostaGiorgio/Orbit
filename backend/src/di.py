11fefca8445f24a1fbb14bb8e4fc009fada68e7f
AgostaGiorgio <agogi.dev@gmail.com>
2026-07-01 18:29:54 +0400

    feat: first backend version

--- backend/src/di.py
+++ backend/src/di.py
@@ -1,7 +1,7 @@
 from dependency_injector import containers, providers
 from src.config.app_config import app_config
 
-
+from src.services.registration import RegistrationService
 
 class Container(containers.DeclarativeContainer):
-    pass
+    registration_service = providers.Singleton(RegistrationService)
